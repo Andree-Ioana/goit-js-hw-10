5beae7a2674ed936fd5ed04b75630a21f45d839c
Andree-Ioana <ioana.vinereanu@stud.etti.upb.ro>
2024-02-05 18:56:26 +0200

    commit

--- parcel-project-template-main/src/index.js
+++ parcel-project-template-main/src/index.js
@@ -1,50 +1,72 @@
-import axios from 'axios';
+import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
-import { fetchBreeds } from './cat-api';
+const breedSelect = document.querySelector(".breed-select");
+const loader = document.querySelector(".loader");
+const error = document.querySelector(".error");
+const catInfo = document.querySelector(".cat-info");
 
-import { fetchCatByBreed } from './cat-api';
+const populateBreeds = () => {
+  loader.style.display = "block";
+  breedSelect.style.display = "none";
+  error.style.display = "none";
 
-const apiKey =
-  'live_uydawoXNcsbhNr1l3Wr17wQRTjPk0cbXD4R0FV4LCAlvcXjaOkzgNL4dllAafpHf';
-axios.defaults.headers.common['x-api-key'] = apiKey;
+  fetchBreeds()
+    .then((breeds) => {
+      breeds.forEach((breed) => {
+        const option = document.createElement("option");
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
 
-const catInfoDiv = document.querySelector('.cat-info');
-const breedSelect = document.querySelector('.breed-select');
-
-fetchBreeds()
-  .then(breeds => {
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.value;
-      option.text = breed.label;
-      breedSelect.appendChild(option);
-    });
-  })
-  .catch(error => {
-    console.error('Error loading breeds:', error);
-  });
-
-breedSelect.addEventListener('change', () => {
-  // obtin valoarea pe care o selectez
-  const selectedBreedId = breedSelect.value;
-
-  // apelez functia  pentru a obtine informații despre pisică
-  fetchCatByBreed(selectedBreedId)
-    .then(catInfo => {
-      // Actualizez div-ul pentru a afișa informați despre pisică
-      catInfoDiv.innerHTML = `
-       <div class="cat">
-       <img src="${catInfo.imageUrl}" alt="Cat Image"  class="cat-image">
-       <div class="cat-container">
-       <p ><b>${catInfo.breed}</b> </p>
-       <p >${catInfo.description}</p>
-       <p><b>Temperament</b>: ${catInfo.temperament}</p>
-       </div>
-       </div>
-      `;
+      breedSelect.style.display = "block";
+      loader.style.display = "none";
     })
-    .catch(error => {
-      console.error('Error fetching cat info:', error);
-      catInfoDiv.innerHTML = '<p>Error fetching cat info</p>';
+    .catch((err) => {
+      loader.style.display = "none";
+      error.style.display = "block";
     });
+};
+
+const displayCatInfo = (breedId) => {
+  loader.style.display = "block";
+  catInfo.style.display = "none";
+  error.style.display = "none";
+
+  fetchCatByBreed(breedId)
+    .then((catData) => {
+      const cat = catData[0];
+      const breedName = cat.breeds[0].name;
+      const description = cat.breeds[0].description;
+      const temperament = cat.breeds[0].temperament;
+      const imageUrl = cat.url;
+
+      catInfo.innerHTML = `
+        <div class = "cat">
+        <img src="${imageUrl}" alt="${breedName}"  class="cat-image">
+       <div class="cat-container">
+       <p><b>${breedName}</b> </p>
+       <p> ${description}</p>
+       <p><b>Temperament:</b> ${temperament}</p>
+       </div>
+        </div>
+      `;
+
+      catInfo.style.display = "block";
+      loader.style.display = "none";
+    })
+    .catch((err) => {
+      loader.style.display = "none";
+      error.style.display = "block";
+    });
+};
+
+
+breedSelect.addEventListener("change", (event) => {
+  const selectedBreedId = event.target.value;
+  displayCatInfo(selectedBreedId);
 });
+
+
+populateBreeds();
